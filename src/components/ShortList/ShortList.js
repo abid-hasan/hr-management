31a5c2d2ef2d36ca06f8a8ca33027336e823bd47
Abid-Hasan <ahsnigdho@gmail.com>
2021-10-29 02:17:54 +0600

    UI done

--- src/components/ShortList/ShortList.js
+++ src/components/ShortList/ShortList.js
@@ -3,8 +3,14 @@ import './Shortlist.css';
 
 const Shortlist = () => {
     return (
-        <div>
-            <h3>Shortlist</h3>
+        <div className="shortlist-container">
+            <h2>Shortlist</h2>
+            <ul>
+                <li>Leanne Graham</li>
+            </ul>
+            <h4>Employee count: 1</h4>
+            <h4>Total Cost: $1000</h4>
+            <button className="add-button">Hire shortlisted applicants</button>
         </div>
     );
 };
